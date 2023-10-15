cb20f9b33a76aa8dbac99ab6fd58285bcab1c9ef
siva tharun <sivatharun2212@gmail.com>
2023-10-15 15:10:21 +0530

    stage 1

--- src/components/stage1/stage1.jsx
+++ src/components/stage1/stage1.jsx
@@ -4,6 +4,8 @@ import axios from "axios";
 import { db } from "../../firebase";
 import { getDoc, doc } from "firebase/firestore";
 import { UserContext } from "../../context/usercontext";
+import styles from "./stage1.module.css";
+
 const Stage1 = ({ aadhaarNumber, setAadhaarNumber, setStage }) => {
 	const [userConsent, setUserConsent] = useState(false);
 
@@ -13,55 +15,72 @@ const Stage1 = ({ aadhaarNumber, setAadhaarNumber, setStage }) => {
 		if (isvalidAadhaar && aadhaarNumber.length === 12 && userConsent === true) {
 			setStage("stage2");
 			console.log(isvalidAadhaar);
-			try {
-				const response = await axios.post(
-					"https://healthidsbx.abdm.gov.in/api/v2/registration/aadhaar/generateOtp",
-					{
-						aadhaar: aadhaarNumber,
-					},
-					{
-						headers: {
-							Authorization: `Bearer ${accessToken}`,
-							"Content-Type": "application/json",
-						},
-					}
-				);
+			// try {
+			// 	const response = await axios.post(
+			// 		"https://healthidsbx.abdm.gov.in/api/v2/registration/aadhaar/generateOtp",
+			// 		{
+			// 			aadhaar: aadhaarNumber,
+			// 		},
+			// 		{
+			// 			headers: {
+			// 				Authorization: `Bearer ${accessToken}`,
+			// 				"Content-Type": "application/json",
+			// 			},
+			// 		}
+			// 	);
 
-				if (response.status === 200) {
-					alert("OTP request successful.");
-				} else {
-					alert("OTP request failed.");
-				}
-			} catch (error) {
-				console.error("Error requesting OTP:", error);
-				alert("Error requesting OTP.");
-			}
+			// 	if (response.status === 200) {
+			// 		alert("OTP request successful.");
+			// 	} else {
+			// 		alert("OTP request failed.");
+			// 	}
+			// } catch (error) {
+			// 	console.error("Error requesting OTP:", error);
+			// 	alert("Error requesting OTP.");
+			// }
 		} else {
 			alert("aadhaar invalid");
 		}
 	};
 	return (
 		<>
-			<p>Aadhar Number</p>
-			<input
-				type="text"
-				placeholder="Enter your aadhar number"
-				value={aadhaarNumber}
-				onChange={(e) => setAadhaarNumber(e.target.value)}
-			/>
-			<div>
-				<h3>Consent Agreement</h3>
-				<p>By proceeding, you agree to the terms and conditions of using the ABHA Number service.</p>
-				<label>
+			<section className={styles.section}>
+				<div className={styles.container}>
+					<p className={styles.aadhaarText}>Aadhar Number</p>
 					<input
-						type="checkbox"
-						checked={userConsent}
-						onChange={() => setUserConsent(!userConsent)}
+						className={styles.input}
+						type="text"
+						placeholder="Enter your aadhar number"
+						value={aadhaarNumber}
+						onChange={(e) => setAadhaarNumber(e.target.value)}
 					/>
-					I agree
-				</label>
-			</div>
-			<button onClick={handleNextClick}>Next</button>
+					<div>
+						<h5>Consent Agreement</h5>
+						<p>
+							By proceeding, you agree to the{" "}
+							<a
+								href="https://uhi.abdm.gov.in/home/terms_condition"
+								target="_blank">
+								terms and conditions
+							</a>{" "}
+							of using the ABHA Number service.
+						</p>
+						<label>
+							<input
+								type="checkbox"
+								checked={userConsent}
+								onChange={() => setUserConsent(!userConsent)}
+							/>
+							I agree
+						</label>
+					</div>
+					<button
+						className={styles.NextBtn}
+						onClick={handleNextClick}>
+						Next
+					</button>
+				</div>
+			</section>
 		</>
 	);
 };
